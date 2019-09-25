19c9845af8c9fe40c505215b93c5cc27f4c1abce
isma-qin <ccarrillo@gmail.com>
2019-09-25 17:56:34 -0300

    create grid component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import './App.scss';
+import Grid from './component/Grid';
 
 function App() {
+  console.log("componentWillMount");
+
+  useEffect(() => {
+    console.log("componentDidMount");
+  }, []); 
+
   return (
     <div className="App">
       <div className="App-header">
+        <Grid />
       </div>
     </div>
   );
---
+++ src/component/Grid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Grid() {
+  return (
+    <div className="grid">
+      huup
+    </div>
+  );
+}
+
+export default Grid;
